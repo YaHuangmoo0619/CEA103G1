1698cca672b1d1044dab345efb4f517b20466657
Jun <ken970201@gmail.com>
2021-05-12 13:23:21 +0800

    登入

--- CEA103G1/src/com/member/controller/MemberServlet.java
+++ CEA103G1/src/com/member/controller/MemberServlet.java
@@ -1,6 +1,8 @@
 package com.member.controller;
 
 import java.io.*;
+import java.net.URLEncoder;
+import java.sql.SQLException;
 import java.text.*;
 import java.util.*;
 
@@ -8,6 +10,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
+import com.member.model.MemberDAO;
 import com.member.model.MemberService;
 import com.member.model.MemberVO;
 
@@ -479,8 +482,95 @@ public class MemberServlet extends HttpServlet {
 				failureView.forward(req, res);
 			}
 		}
-	
+		
+		if ("login_Member".equals(action)) { // 來自select_page.jsp的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				String acc = req.getParameter("acc");
+				if (acc == null || (acc.trim()).length() == 0) {
+					errorMsgs.add("請輸入帳號");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front-end/member/login.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				String pwd = req.getParameter("pwd");
+				if (pwd == null || (pwd.trim()).length() == 0) {
+					errorMsgs.add("請輸入密碼");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front-end/member/login.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				String acc1 = null;
+				try {
+					acc1 = new String(acc1);
+				} catch (Exception e) {
+					errorMsgs.add("帳號格式不正確");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front-end/member/login.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				String pwd1 = null;
+				try {
+					pwd1 = new String(pwd1);
+				} catch (Exception e) {
+					errorMsgs.add("密碼格式不正確");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front-end/member/login.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				/***************************2.開始查詢資料*****************************************/
+				MemberService memberSvc = new MemberService();
+				MemberVO memberVO = memberSvc.loginMember(acc, pwd);
+				if (memberVO == null) {
+					errorMsgs.add("查無資料");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front/member/login.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+		
+				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("MemberVO", memberVO); // 資料庫取出的member_rankVO物件,存入req
+				String url = "/front-end/member/campion_front.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 campion_front.jsp
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/front-end/member/login.jsp");
+				failureView.forward(req, res);
+			}
+		}
 	}
-
 }
-
--- CEA103G1/src/com/member/model/MemberDAO_interface.java
+++ CEA103G1/src/com/member/model/MemberDAO_interface.java
@@ -1,7 +1,5 @@
 package com.member.model;
 
-import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.List;
 
 public interface MemberDAO_interface {
@@ -9,11 +7,11 @@ public interface MemberDAO_interface {
           public void insert(MemberVO memberVO);
           public void update(MemberVO memberVO);
           public void delete(Integer mbr_no);
+          public void register_member(MemberVO memberVO);
           public MemberVO findByPrimaryKey(Integer mbr_no);
+          public MemberVO findByPrimaryKey_login(String acc, String pwd);
           public List<MemberVO> getAll();
-          public List<MemberVO> getDateMbr_no(Date bday);
-          public List<MemberVO> getTimestampMbr_no(Timestamp join_time);
+
           //萬用複合查詢(傳入參數型態Map)(回傳 List)
-//        public List<MemberVO> getAll(Map<String, String[]> map);
-		
+//        public List<MemberVO> getAll(Map<String, String[]> map)	
 }
