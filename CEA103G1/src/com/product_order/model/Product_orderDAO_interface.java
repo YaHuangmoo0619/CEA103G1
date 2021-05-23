80304f704dd7faaac9ad764b09934c5e55dd875f
YaHuangmoo0619 <klfm1224@gmail.com>
2021-05-23 23:23:14 +0800

    去掉衝突標記

--- CEA103G1/src/com/product_order/model/Product_orderDAO_interface.java
+++ CEA103G1/src/com/product_order/model/Product_orderDAO_interface.java
@@ -8,11 +8,11 @@ public interface Product_orderDAO_interface {
      public void delete(Integer prod_ord_no);
      public Product_orderVO findByPrimaryKey(Integer prod_ord_no);
      public List<Product_orderVO> getAll();
-<<<<<<< HEAD
+
      //雅凰加的
      public void update_order_stat(Integer prod_ord_stat, Integer prod_ord_no);
      //雅凰加的
-=======
+
      public List<Product_orderVO> getAllByMbr();
->>>>>>> 39c78c37739c60e8e9bc21573896ec60409b4f4e
+
 }
