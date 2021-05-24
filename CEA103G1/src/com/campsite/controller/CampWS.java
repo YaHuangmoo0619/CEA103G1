5bb1b62de463a2f6172ca1ca0f3ff51fefca1c89
fish12422 <fish12422@gmail.com>
2021-05-24 15:37:26 +0800

    更新一點搶位系統，也碰到一點bug，可惡

--- CEA103G1/src/com/campsite/controller/CampWS.java
+++ CEA103G1/src/com/campsite/controller/CampWS.java
@@ -20,16 +20,25 @@ import javax.websocket.server.ServerEndpoint;
 public class CampWS {
 	private static Map<String, Set<Session>> camps = new ConcurrentHashMap();
 	private static HashMap<String, Set<String>> history = new HashMap();
+	private static HashMap<String, HashMap<Session, Set<String>>> forDisconnect = new HashMap();
 
 	@OnOpen
 	public void onOpen(@PathParam("campInfo") String campInfo, Session session) throws IOException {
-		System.out.println("WS~~~~~");
+		System.out.println("connect:" + campInfo + " " + session);
 		if (!camps.containsKey(campInfo)) {
 			Set<Session> camp = new HashSet<Session>();
 			camp.add(session);
 			camps.put(campInfo, camp);
+
+			HashMap<Session, Set<String>> forDis = new HashMap<Session, Set<String>>();
+			forDis.put(session, null);
+			forDisconnect.put(campInfo, forDis);
 		} else {
 			camps.get(campInfo).add(session);
+
+			HashMap<Session, Set<String>> forDis = forDisconnect.get(campInfo);
+			forDis.put(session, null);
+			forDisconnect.put(campInfo, forDis);
 		}
 		Set<String> plc = history.get(campInfo);
 		if (plc != null) {
@@ -47,6 +56,56 @@ public class CampWS {
 
 	@OnClose
 	public void disConnect(@PathParam("campInfo") String campInfo, Session session) {
+for (Session receiver : camps.get(campInfo)) {
+if (receiver != session) {
+System.out.println(receiver + ":" + forDisconnect.get(campInfo).get(receiver));
+}}
+		System.out.println("disconnect:" + campInfo + " " + session);
+		Set<String> forDisPlc = forDisconnect.get(campInfo).get(session);
+		System.out.println("disconnect:" + forDisPlc);
+		if (forDisPlc != null) {
+			String disPlc = forDisPlc.toString().substring(1, forDisPlc.toString().length() - 1);
+			if (!disPlc.contains(", ")) {
+				Set<String> left = history.get(campInfo);
+				left.remove(disPlc);
+				history.put(campInfo, left);
+				for (Session receiver : camps.get(campInfo)) {
+					receiver.getAsyncRemote().sendText(disPlc + "&yes");
+				}
+			} else {
+				String[] remove = disPlc.split(", ");
+				String[] historyPlc = (history.get(campInfo).toString().substring(1,
+						history.get(campInfo).toString().length() - 1)).split(", ");
+
+				for (int i = 0; i < historyPlc.length; i++) {
+					for (int j = 0; j < remove.length; j++) {
+						if (historyPlc[i].equals(remove[j])) {
+							historyPlc[i] = null;
+							break;
+						}
+					}
+				}
+				Set<String> left = new HashSet();
+				for (int i = 0; i < historyPlc.length; i++) {
+					if (historyPlc[i] != null) {
+						left.add(historyPlc[i]);
+					}
+				}
+				System.out.println("disconnect:" + "剩下的" + left);
+				history.put(campInfo, left);
+				disPlc = disPlc.replaceAll(", ", "&dis&");
+				System.out.println(disPlc);
+				for (Session receiver : camps.get(campInfo)) {
+					if (receiver != session) {
+						receiver.getAsyncRemote().sendText(disPlc);
+					}
+				}
+			}
+		}
+for (Session receiver : camps.get(campInfo)) {
+if (receiver != session) {
+System.out.println(receiver + ":" + forDisconnect.get(campInfo).get(receiver));
+}}
 		camps.get(campInfo).remove(session);
 		if (camps.get(campInfo).size() == 0) {
 			history.put(campInfo, null);
@@ -57,14 +116,24 @@ public class CampWS {
 	@OnMessage
 	public void receiveMsg(@PathParam("campInfo") String campInfo, String plc_no, Session session) throws Exception {
 		System.out.println(plc_no);
-		System.out.println(campInfo);
+		System.out.println(campInfo + " " + session);
 		if (plc_no.contains("release")) {
 			plc_no = plc_no.split("&")[0];
 			Set<String> historyPlc = history.get(campInfo);
-			System.out.println(historyPlc);
+			System.out.println("移除前" + historyPlc);
 			historyPlc.remove(plc_no);
-			System.out.println(historyPlc);
+			System.out.println("移除後" + historyPlc);
 			history.put(campInfo, historyPlc);
+
+			Set<String> forDisPlc = forDisconnect.get(campInfo).get(session);
+			System.out.println("我有執行1");
+			forDisPlc.remove(plc_no);
+			System.out.println("我有執行2");
+			HashMap<Session, Set<String>> forDisSession = new HashMap<Session, Set<String>>();
+			forDisSession.put(session, forDisPlc);
+			forDisconnect.put(campInfo, forDisSession);
+			System.out.println("離線用" + forDisconnect.get(campInfo).get(session));
+
 			for (Session receiver : camps.get(campInfo)) {
 				if (receiver != session) {
 					receiver.getAsyncRemote().sendText(plc_no + "&yes");
@@ -78,15 +147,28 @@ public class CampWS {
 			}
 		} else {
 			Set<String> historyPlc = history.get(campInfo);
-			System.out.println(historyPlc);
+			System.out.println("新增前" + historyPlc);
 			if (historyPlc == null) {
 				historyPlc = new HashSet<String>();
 				historyPlc.add(plc_no);
 			} else {
 				historyPlc.add(plc_no);
 			}
-			System.out.println(historyPlc);
+			System.out.println("新增後" + historyPlc);
 			history.put(campInfo, historyPlc);
+
+			Set<String> forDisPlc = forDisconnect.get(campInfo).get(session);
+			if (forDisPlc == null) {
+				forDisPlc = new HashSet<String>();
+				forDisPlc.add(plc_no);
+			} else {
+				forDisPlc.add(plc_no);
+			}
+			HashMap<Session, Set<String>> forDisSession = new HashMap<Session, Set<String>>();
+			forDisSession.put(session, forDisPlc);
+			forDisconnect.put(campInfo, forDisSession);
+			System.out.println("離線用" + forDisconnect.get(campInfo).get(session));
+
 			for (Session receiver : camps.get(campInfo)) {
 				if (receiver != session) {
 					receiver.getAsyncRemote().sendText(plc_no + "&no");
