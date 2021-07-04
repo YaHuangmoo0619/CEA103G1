26bf40f2e09d1059c63004000bd24574de766bea
YaHuangmoo0619 <81008699+YaHuangmoo0619@users.noreply.github.com>
2021-07-04 16:08:43 +0800

    Update FunctionVO.java
    為了使用hibernate所以加上annotation

--- CEA103G1/src/com/function/model/FunctionVO.java
+++ CEA103G1/src/com/function/model/FunctionVO.java
@@ -1,9 +1,43 @@
 package com.function.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.employee.model.EmployeeVO;
+
+@Entity
+@Table(name="`function`")
 public class FunctionVO implements java.io.Serializable{
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="fx_no")
 	private Integer fx_no;
+	
+	@Column(name="fx_name")
 	private String fx_name;
+	
+	@ManyToMany(fetch=FetchType.LAZY,
+			cascade= {CascadeType.DETACH, CascadeType.MERGE,
+					CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinTable(
+			name="authority",
+			joinColumns=@JoinColumn(name="fx_no"),
+			inverseJoinColumns=@JoinColumn(name="emp_no"))
+	private List<EmployeeVO> employeeVO;
 	
 	public FunctionVO() {
 	}
@@ -28,5 +62,14 @@ public class FunctionVO implements java.io.Serializable{
 	public void setFx_name(String fx_name) {
 		this.fx_name = fx_name;
 	}
+
+	public List<EmployeeVO> getEmployeeVO() {
+		return employeeVO;
+	}
+
+	public void setEmployeeVO(List<EmployeeVO> employeeVO) {
+		this.employeeVO = employeeVO;
+	}
+	
 	
 }
